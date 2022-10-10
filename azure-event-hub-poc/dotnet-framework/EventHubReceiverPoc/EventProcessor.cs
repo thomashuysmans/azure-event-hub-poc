8ce086cac87f3220139ac6688496676dcb122ae3
Thomas Huysmans <thomas@kaizenit.consulting>
2022-10-10 16:30:42 +0200

    Dotnet Framework

---
+++ azure-event-hub-poc/dotnet-framework/EventHubReceiverPoc/EventProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHubReceiverPoc
+{
+    internal class EventProcessor : IEventProcessor
+    {
+        public Task OpenAsync(PartitionContext context)
+        {
+            return Task.FromResult(true);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+
+            EventData lastMessage = null;
+            //ServiceEventSource.Current.EventProcessorProcessing(context, messages);
+
+            try
+            {
+                foreach (var message in messages)
+                {
+                    var payload = Encoding.UTF8.GetString(message.GetBytes());
+                    
+                    if (payload == "Event 74")
+                    {
+                        Console.WriteLine($"Something with handling {payload}");
+                        throw new Exception("Something went wrong.");
+                    }
+                    
+                    Console.WriteLine(payload);
+                    lastMessage = message;
+                }
+
+                if (lastMessage != null && context != null)
+                {
+                    await context.CheckpointAsync(lastMessage);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            await Task.FromResult(true);
+        }
+
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ azure-event-hub-poc/dotnet-framework/EventHubReceiverPoc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHubReceiverPoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start the application:");
+
+            var eventHubConnectionString = "Endpoint=sb://kaizenit.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4+FxOvzDqdv5giZtXa8qCYbKHWtBcvleCfh8urEogXk="; ;
+            var eventHubName = "myfirsteventhub";
+            var consumerGroupName = "$Default";
+            var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=kaizenitstorage;AccountKey=N/0hcL0SAZVCmPWoTWC/tYDmM7Da8sQ55qN4vI+QDDyUGwecl1Ff9qUFhm/sSQtqOtKBamF/oILe2uoXsIefZA==;EndpointSuffix=core.windows.net";
+
+            var eventProcessorHost = new EventProcessorHost(eventHubPath:eventHubName, 
+                consumerGroupName:consumerGroupName, eventHubConnectionString: eventHubConnectionString, storageConnectionString: blobStorageConnectionString);
+
+            eventProcessorHost.RegisterEventProcessorAsync<EventProcessor>(new EventProcessorOptions()
+            {
+                MaxBatchSize = 32
+            });
+
+            
+            Console.ReadLine();
+        }
+    }
+}
